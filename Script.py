28633cee00132f7e87d17b6b5bad70bdf0a10012
trapperboy <92577580+trapperboy@users.noreply.github.com>
2021-11-11 20:22:36 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ class script(object):
 - Source - https://t.me/bettermalayalam  
 
 <b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+- <a href=https://t.me/bettermalayalam>Team Eva Maria</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
